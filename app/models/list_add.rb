bdfa42bdaf1a4a1fe2b8320c8011537286e6fefe
MattBarnabo <mattbarnabo@gmail.com>
2024-04-24 17:12:30 -0500

    adds model list validations and tests

--- app/models/list_add.rb
+++ app/models/list_add.rb
@@ -1,3 +1,7 @@
 class ListAdd < ApplicationRecord
+  validates :rating, :watched, :tmdb_api_id, presence: true
+  validates :rating, inclusion: { in: 0..5 }, allow_nil: true
+  validates :watched, inclusion: [true, false]
+  validates :watched, exclusion: [nil]
   belongs_to :user
 end
